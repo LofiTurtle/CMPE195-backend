516dc6f0bc532ddf86aa3d5299316ce9901d6d69
John Clear <johnclear4@gmail.com>
2024-02-28 15:22:04 -0800

    Add example API route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/api/hello')
+def api_hello():
+    return jsonify(data='Hello from the flask API')
 
 
 if __name__ == '__main__':
