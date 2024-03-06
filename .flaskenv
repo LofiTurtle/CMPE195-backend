bc16484cf74f18935a3cfdcb1240256d10e94710
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2024-03-06 14:56:53 -0800

    Update configuration

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 FLASK_APP=server
 FLASK_ENV=development
+REACT_APP_URL=http://localhost:5173
--- server/__init__.py
+++ server/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Configuration
 app.config.from_pyfile('config.py')
 
 import server.routes
