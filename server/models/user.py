9539e555b687639fbf856fb077f3ac7338259e1d
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2024-12-02 16:51:00 -0800

    Fix foreign key issue

--- server/models/user.py
+++ server/models/user.py
@@ -30,7 +30,7 @@ class User(db.Model):
     communities = db.relationship('Community', secondary='user_communities', back_populates='users')
     owned_communities = db.relationship('Community', back_populates='owner')
     given_ratings = db.relationship('Rating', back_populates='rating_user', foreign_keys='[Rating.rating_user_id]')
-    received_ratings = db.relationship('Rating', back_populates='rated_user', foreign_keys='[Rating.rating_user_id]')
+    received_ratings = db.relationship('Rating', back_populates='rated_user', foreign_keys='[Rating.rated_user_id]')
     following = db.relationship(
         'User',
         secondary=user_following,
