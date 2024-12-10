303d8db67ab845749b3b81404a6733ec6d87aeff
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2024-12-10 12:33:03 -0800

    Update image handling

--- server/services/media_processing.py
+++ server/services/media_processing.py
@@ -7,31 +7,26 @@ from flask import current_app
 from server.models import User
 
 
-def save_image(image, max_size: int = 512) -> str:
-    """Save a profile picture and return its UUID"""
+def save_image(image, max_size: int = 1024) -> str:
     pfp_uuid = str(uuid.uuid4())
     filename = pfp_uuid + '.jpg'
     filepath = os.path.abspath(os.path.join(current_app.config['UPLOAD_DIRECTORY'], filename))
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
     pfp = Image.open(image)
-
     width, height = pfp.size
-    size = min(width, height)
-    left = (width - size) // 2
-    top = (height - size) // 2
-    right = left + size
-    bottom = top + size
-    pfp = pfp.crop((left, top, right, bottom))
 
-    if size > max_size:
-        pfp = pfp.resize((max_size, max_size))
+    ratio = min(max_size / width, max_size / height)
+    new_width = int(width * ratio)
+    new_height = int(height * ratio)
+
+    if ratio < 1:
+        pfp = pfp.resize((new_width, new_height))
 
     if pfp.mode != 'RGB':
         pfp = pfp.convert('RGB')
 
     pfp.save(filepath, 'JPEG')
-
     return pfp_uuid
 
 
